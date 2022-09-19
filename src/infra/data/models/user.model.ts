af8df039d5a511fe3740ccb281e761a167de8145
william soares <ext.william.lima@burgerking.com.br>
2022-09-19 20:45:32 -0300

    feat: encrypt password

--- src/infra/data/models/user.model.ts
+++ src/infra/data/models/user.model.ts
@@ -1,3 +1,4 @@
+import * as bcrypt from 'bcrypt';
 import { model, Model, Schema } from "mongoose";
 
 export interface IUserDbModel {
@@ -11,5 +12,23 @@ const UserSchema = new Schema<IUserDbModel>({
   email: { type: String, required: true },
   password: { type: String, required: true },
 });
+
+UserSchema.pre("save", async function (next) {
+  const user = this
+
+  if (this.isModified("password") || this.isNew) {
+    try {
+      const hash = await bcrypt.hash(user.password, 8);
+      
+      user.password = hash
+
+      next();
+    } catch (error) {
+      return next(error)
+    }
+  } else {
+    return next()
+  }
+})
 
 export const UserDbModel: Model<IUserDbModel> = model('users', UserSchema);
